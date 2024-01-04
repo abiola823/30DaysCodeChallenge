73c1467db0ea5638231e964bb68be29cf79f048c
olaide <olaideabiola2@gmail.com>
2024-01-04 19:50:47 +0100

    added day 14 task conatining user role

--- Day13/Model/model.js
+++ Day13/Model/model.js
@@ -16,6 +16,12 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["user", "admin"],
+      default: "user"
     }
 },{
   timestamps: true
